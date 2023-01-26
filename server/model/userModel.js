8cb5e13c27016177ec303388497948e8a74a1dc8
Talha Rauf <bsee16024@itu.edu.pk>
2023-01-26 11:26:41 +0500

    incorporating auth in ejs files

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -2,11 +2,19 @@ const mongoose = require("mongoose");
 
 var mySchema = new mongoose.Schema({
 
-    name:{
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
         type: String,
         required: true
     },
     email:{
+        type: String,
+        required: true
+    },
+    password:{
         type: String,
         required: true
     },
